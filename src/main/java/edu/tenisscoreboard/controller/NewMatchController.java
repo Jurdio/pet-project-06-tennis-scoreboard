adff046f5a010db200706e257e1e3fff4b0da128
Dmytro <dmytro.kupets@gmail.com>
2024-01-29 19:36:27 +0200

    add EncodingFilter.java and write new-match.jsp

--- src/main/java/edu/tenisscoreboard/controller/NewMatchController.java
+++ src/main/java/edu/tenisscoreboard/controller/NewMatchController.java
@@ -13,6 +13,6 @@ public class NewMatchController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        
     }
 }
